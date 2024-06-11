b0a84db17515fc02013a473fdfc1871324bed348
Matin Ansaripour <matinansaripour@gmail.com>
2024-06-11 08:38:35 +0200

    fix tune_hyperparameters.sh

--- tune_hyperparameters.sh
+++ tune_hyperparameters.sh
@@ -5,18 +5,18 @@ PROJECT_NAME="bitNet_gradient_free"
 run_sweep_and_agent () {
   # Set the SWEEP_NAME variable
   SWEEP_NAME="$1"
-  
+
   # Run the wandb sweep command and store the output in a temporary file
   wandb sweep --project "$PROJECT_NAME" --name "$SWEEP_NAME" "hyperparams_opt/$SWEEP_NAME.yaml" >temp_output.txt 2>&1
-  
+
   # Extract the sweep ID using awk
   SWEEP_ID=$(awk '/wandb agent/{ match($0, /wandb agent (.+)/, arr); print arr[1]; }' temp_output.txt)
   
   # Remove the temporary output file
   rm temp_output.txt
-  
+
   # Run the wandb agent command
-  wandb agent --count 10 $SWEEP_ID
+  wandb agent --count 10 "$SWEEP_ID" --project "$PROJECT_NAME" --entity "optML"
 }
 
 # list of sweeps to call
